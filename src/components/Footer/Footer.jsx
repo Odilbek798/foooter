1e494868c927edcd81c7a746481ecb0074742234
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2024-12-29 17:38:55 +0500

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <div className="last">
+            <div className="quick">
+                <h3>Quickly.</h3>
+                <a href="">32  - 44 Upper Rupert Street</a>
+                <a href="">W12 8DY Kensington</a>
+                <a href="">United Kingdom</a>
+            </div>
+            <div className="our">
+                <div className="company">
+                    <h3>Our Company</h3>
+                    <a href="">Our Team</a>
+                    <a href="">Company Vision</a>
+                    <a href="">Our History</a>
+                    <a href="">Investorys</a>
+                </div>
+                <div className="company">
+                    <h3>Our Company</h3>
+                    <a href="">Our Team</a>
+                    <a href="">Company Vision</a>
+                    <a href="">Our History</a>
+                    <a href="">Investorys</a>
+                </div>
+                <div className="company">
+                    <h3>Our Company</h3>
+                    <a href="">Our Team</a>
+                    <a href="">Company Vision</a>
+                    <a href="">Our History</a>
+                    <a href="">Investorys</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
